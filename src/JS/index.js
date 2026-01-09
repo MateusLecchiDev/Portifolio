238a47b440841e815802ab1982660a5be87f5553
Mateus Lecchi <mateusrlecchi@gmail.com>
2026-01-09 15:09:39 -0300

    Retirado fundo da imagem de perfil, implementado um botão para mudar o idioma dos textos da página.

--- src/JS/index.js
+++ src/JS/index.js
@@ -16,3 +16,37 @@ function mostrarMaisProjetos() {
         projetoInativo.classList.add('ativo');
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-language');
+    const elements = document.querySelectorAll('[data-pt][data-en]');
+
+    let currentLanguage = 'pt';
+
+    function applyLanguage(lang) {
+        elements.forEach(el => {
+            if (el.tagName === "A" && el.querySelector("i")){
+                el.setAttribute("title", el.dataset[lang]);
+            } else {
+                el.textContent = el.dataset[lang]
+            }
+        });
+
+        if (lang === "pt") {
+            toggleButton.textContent = "Mudar para Inglês";
+            document.documentElement.lang = "pt-BR";
+        } else {
+            toggleButton.textContent = "Switch to Portuguese";
+            document.documentElement.lang = "en-US";
+        }
+
+        currentLanguage = lang;
+    }
+
+    applyLanguage("pt");
+
+    toggleButton.addEventListener('click', () => {
+        const newLang = currentLanguage === "pt" ? "en" : "pt";
+        applyLanguage(newLang);
+    });
+});
